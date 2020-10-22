37519252d52498d7df5f147ee4bd8c5cb806d498
Karol <Karol@DESKTOP-J37B9FF>
2020-10-22 17:36:06 +0200

    dodano changelog

--- OdlegloscLevenshteina/Asm/LibAsm.asm
+++ OdlegloscLevenshteina/Asm/LibAsm.asm
@@ -1,3 +1,15 @@
+
+;								Odleg³oœæ Levenshteina v 0.1.3
+;		
+;										Karol Krzywoñ
+;
+;	Changelog:
+;	- 15.10.2020 - v 0.1.1 - utoworzenie projektu, pocz¹tkowe UI
+;	- 18.10.2020 - v 0.1.2 - poprawa UI, stworzenie repozytorium na GitHubie
+;	- 22.10.2020 - v 0.1.3 - dalsza poprawa GUI - ³adowanie plików wejœciowych i wyjœciowych,
+;							 wczytywanie s³ow do porównania, "uaktywnienie" przycisków,
+;							 stworzenie DLL dla jêzyka asemblerowego i C++
+
 .code					
 init proc					
 	mov rax, 2
